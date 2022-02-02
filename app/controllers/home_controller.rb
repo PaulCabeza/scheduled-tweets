63654a351b9ae15d826dfa6e79e5d9823f0a1ac3
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-02 17:13:41 -0600

    Setup flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    flash.now[:notice] = 'Logged in Successfully'
+    flash.now[:alert] = 'Invalid email or password'
+  end
+
+  def about
   end
 end
