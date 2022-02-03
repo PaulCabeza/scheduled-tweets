d813166cd495abb251c350e87b22b6097fee4c76
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-03 16:40:16 -0600

    Added save new user feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # validates :email, presence: true
+  validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: 'Not a valid email'}
 end
