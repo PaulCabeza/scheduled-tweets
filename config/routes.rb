ec559dd20414555d45e4fd119fc2d9cff867abbe
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-03 22:48:22 -0600

    Added login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
   get 'sign_up', to: "registrations#new"
   post 'sign_up', to: 'registrations#create'
   delete 'logout', to: 'sessions#destroy'
+
+  get 'sign_in', to: "sessions#new"
+  post 'sign_in', to: 'sessions#create'
 
   get 'about', to:"home#about"
 end
