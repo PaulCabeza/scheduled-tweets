6b5125eedeffda26bb20faeb09b95b456d7b303e
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-02-05 22:54:48 -0600

    Change password feature added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
 
+  def require_user_logged_in!
+    redirect_to login_path, alert: "You must be signed in." if Current.user.nil?
+
+  end
+
   def set_current_user
     if session[:user_id]
       Current.user = User.find(session[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
+  get "password-reset", to: "passwords#edit"
+  patch "password-reset", to: "passwords#update"
+
   get 'sign_up', to: "registrations#new"
   post 'sign_up', to: 'registrations#create'
   delete 'logout', to: 'sessions#destroy'
